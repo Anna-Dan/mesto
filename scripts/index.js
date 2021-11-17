0e4381db2027f7a78e4a810fa0e90912d32455d4
Anna Danilenko <danilenko.anna2016@yandex.ru>
2021-11-17 20:05:45 +0300

    feat: implemented adding a card

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,9 @@ const closeEditButton = popupEditElement.querySelector(".popup__close-button");
 const saveEditButton = popupEditElement.querySelector(".popup__save-button");
 const formEditElement = document.querySelector(".popup__form_type_edit");
 const nameInput = formEditElement.querySelector(".popup__input_type_name");
-const jobInput = formEditElement.querySelector(".popup__input_type_description");
+const jobInput = formEditElement.querySelector(
+  ".popup__input_type_description"
+);
 // Add popup
 const popupAddElement = document.querySelector(".popup_type_add");
 const closeAddButton = popupAddElement.querySelector(".popup__close-button");
@@ -12,17 +14,12 @@ const saveAddButton = popupAddElement.querySelector(".popup__save-button");
 const formAddElement = document.querySelector(".popup__form_type_add");
 const placeInput = formAddElement.querySelector(".popup__input_type_place");
 const urlInput = formAddElement.querySelector(".popup__input_type_url");
-
 // Профиль
 const profile = document.querySelector(".profile");
 const addButton = profile.querySelector(".profile__add-button");
 const editButton = profile.querySelector(".profile__edit-button");
 const profileName = profile.querySelector(".profile__name");
 const profileDescription = profile.querySelector(".profile__description");
-
-// Заполнение данными со страницы при открытии формы редактирования профиля
-nameInput.value = profileName.textContent;
-jobInput.value = profileDescription.textContent;
 
 // Функция открытие попапа
 function openPopup(popupElement) {
@@ -32,15 +29,11 @@ function openPopup(popupElement) {
 function closePopup(popupElement) {
   popupElement.classList.remove("popup_opened");
 }
-// Обработчик сохранения данных пользователя
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
-  closePopup(popupEditElement);
-}
-// Сохранить данные пользователя
-formEditElement.addEventListener("submit", formSubmitHandler);
+
+//  Edit popup
+// Заполнение данными со страницы при открытии формы редактирования профиля
+nameInput.value = profileName.textContent;
+jobInput.value = profileDescription.textContent;
 
 // Открыть попап редактирования профиля
 editButton.addEventListener("click", () => {
@@ -52,15 +45,42 @@ closeEditButton.addEventListener("click", () => {
   closePopup(popupEditElement);
 });
 
-// Открыть попап добавления места
+// Обработчик сохранения данных пользователя
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closePopup(popupEditElement);
+}
+// Сохранить данные пользователя
+formEditElement.addEventListener("submit", formSubmitHandler);
+
+// Add popup
+// Открыть попап добавления карточки места
 addButton.addEventListener("click", () => {
   openPopup(popupAddElement);
 });
 
-// Закрыть попап добавления места
+// Закрыть попап добавления карточки места
 closeAddButton.addEventListener("click", () => {
   closePopup(popupAddElement);
 });
+
+// Обработчик сохранения карточки места
+function formAddSubmitHandler(evt) {
+  evt.preventDefault();
+  const userCardElement = createElementDomNode({
+    name: placeInput.value,
+    link: urlInput.value,
+  });
+  elementsList.prepend(userCardElement);
+  placeInput.value = '';
+  urlInput.value = '';
+  closePopup(popupAddElement);
+}
+
+// Сохранить карточку места
+formAddElement.addEventListener("submit", formAddSubmitHandler);
 
 // Первоначальное отображение 6 карточек
 const initialCards = [
